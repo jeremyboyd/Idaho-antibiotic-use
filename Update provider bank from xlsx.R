657e05f16a268bdc4bb45af77fbb409f450a1b66
Jeremy Boyd <jeremyboyd@pm.me>
2022-05-05 10:16:26 -0600

    Read data from Karl's most recent xlsx. Read in columns representing both of Karl's categorizations--Std_Provider_Type, and Std_Provider_Type2.

--- Update provider bank from xlsx.R
+++ Update provider bank from xlsx.R
@@ -6,8 +6,10 @@ box_dir <- "~/Library/CloudStorage/Box-Box/IDHW_2022_Idaho_Antibiotic_project/"
 read_xlsx(
     paste0(
         box_dir,
-        "Idaho 2019 providers by prescriber data with address names.xlsx"),
+        "Idaho 2019 providers by prescriber data with address names_kmk.xlsx"),
     sheet = "Provder_Specialty_by_Count") %>%
-    mutate(across(where(is.character), ~ str_squish(.))) %>%
-    select(Prscrbr_Type, Standard_Provider_Type) %>%
+    rename(Std_Provider_Type2 = ...3) %>%
+    mutate(across(where(is.character), ~ str_squish(.)),
+           year = 2019L) %>%
+    select(Prscrbr_Type, year, Std_Provider_Type, Std_Provider_Type2) %>%
     write_feather("Idaho 2019 provider type bank.feather")
